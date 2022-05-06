db2a04c03fd1db2794fba5cf6a624d9fd74f0859
Helias <stefanoborzi32@gmail.com>
2022-05-06 18:44:54 +0200

    chore: apply prettier with new conf

--- static/js/sync-url.js
+++ static/js/sync-url.js
@@ -1,6 +1,6 @@
 window.parent.postMessage(
-  {
-    url: window.location.pathname.replace(handlebarsData.websiteRoot, ''),
-  },
-  '*',
+	{
+		url: window.location.pathname.replace(handlebarsData.websiteRoot, ""),
+	},
+	"*",
 );
